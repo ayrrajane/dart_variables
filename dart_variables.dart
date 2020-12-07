2e8099eb0cd798746090d3bb0a40ed30c224b494
Ayrra Jane Amio <ajamio@wvsu.edu.ph>
2020-12-07 19:06:35 +0800

    Add files via upload

---
+++ dart_variables.dart
@@ -0,0 +1,13 @@
+void main() {
+  String firstName = "Jollibee";
+  String middleInitial = "E";
+  String lastName = "Lopez";
+  int age = 42;
+  double height = 1.83;
+
+  print(firstName);
+  print(middleInitial);
+  print(lastName);
+  print(age);
+  print(height);
+}
